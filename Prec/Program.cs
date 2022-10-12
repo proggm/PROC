0ad893ca4bc1dcac56cfe14584660dbf1be62450
viktor <moiseev0506@mail.ru>
2022-10-12 23:40:48 +0300

    version 1

---
+++ Prec/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prec
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите необходимое вам число: ");
+            double first = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите необходимый вам процент: ");
+            double second = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Вывод: " + Percent(first, second));
+
+            Console.ReadKey();
+
+        }
+        public static double Percent(double one, double two)
+        {
+            return (one * two) / 100;
+        }
+    }
+}
